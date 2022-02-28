2aaaab7f244ead7f2cc8138c0fc769dc14d6740f
WalajaSaiBhargavi <saibhargavi1510@gmail.com>
2022-02-28 15:45:05 +0530

    sort a stack

---
+++ src/dataStructures/stack/SortAStack.java
@@ -0,0 +1,43 @@
+package dataStructures.stack;
+
+import java.util.*;
+
+public class SortAStack {
+	public static void main(String[] args) {
+		Stack<Integer> myStack = new Stack<>();
+		myStack.push(5);
+		myStack.push(8);
+		myStack.push(7);
+		myStack.push(2);
+		myStack.push(0);
+		myStack.push(9);
+		myStack.push(11);
+		sortStack(myStack);
+		for(int i : myStack) {
+			System.out.print(i);
+		}
+	}
+
+	public static void sortStack(Stack<Integer> myStack) {
+		if(!myStack.isEmpty()) {
+			int temp = myStack.pop();
+			sortStack(myStack);
+			sortStack(myStack, temp);
+		}
+	}
+
+	private static void sortStack(Stack<Integer> myStack, int temp) {
+		if(myStack.isEmpty()) {
+			myStack.push(temp);
+		} else {
+			int a = myStack.pop();
+			if(a > temp) {
+				sortStack(myStack, temp);
+				myStack.push(a);
+			} else {
+				sortStack(myStack, a);
+				myStack.push(temp);
+			}
+		}
+	}
+}
