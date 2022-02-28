ca5fe735ac3f2813e8fd2d31789e6a8931e72dd0
Sai Bhargavi Walaja <saibhargavi.walaja@blueyonder.com>
2022-02-28 13:11:02 +0530

    Reverse a Stack without using loop and using recursion

---
+++ src/dataStructures/stack/ReverseAStack.java
@@ -0,0 +1,46 @@
+// Java code to reverse a
+// stack using recursion
+package dataStructures.stack;
+
+import java.util.Stack;
+
+/**
+ * 
+ * Reverse a Stack without using loop and using recursion
+ * Two methods - One gives 
+ *
+ */
+
+class ReverseAStack {
+
+	static void insert_at_bottom(char x, Stack<Character> st) {
+		if (st.isEmpty()) {
+			st.push(x);
+		} else {
+			char temp = st.pop();
+			insert_at_bottom(x, st);
+			st.push(temp);
+		}
+	}
+
+	static void reverse(Stack<Character> st) {
+		if (!st.isEmpty()) {
+			char temp = st.pop();
+			reverse(st);
+			insert_at_bottom(temp, st);
+		}
+	}
+
+	public static void main(String[] args) {
+		Stack<Character> st = new Stack<>();
+		st.push('1');
+		st.push('2');
+		st.push('3');
+		st.push('4');
+		System.out.println("Original Stack");
+		System.out.println(st);
+		reverse(st);
+		System.out.println("Reversed Stack");
+		System.out.println(st);
+	}
+}
